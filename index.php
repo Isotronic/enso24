d38b2827856b685fbfc27d817e531c2dcc519634
Joseph Bouqdib <work@bss-ims.de>
2013-12-28 16:55:36 +0100

    update to basic layout

--- index.php
+++ index.php
@@ -14,24 +14,28 @@
 <title>crm-enso24</title>
 </head>
 <body>
-	<div id="wrapper">
-	    <div id="header">
-            <h2>B&amp;S Solutions CRM</h2>
-            <!--<img src="includes/images/logo_final.png" alt="enso24 logo" width="400" height="100"/>-->
-        </div>
-        <div id="nav">
-            <a href="index.php?pid=default">Überblick</a>
-            <a href="index.php?pid=customer">Bestandskunden</a>
-        </div>
-        <div>
-        	<?php
-        		//this pulls the add new client button
-        		include("pages/new_client/default.php");
-        	?>
-        </div>
-        <div id="footer">
-            <p><!--<a href="../class/logout.php">Logout</a> &nbsp; -->&copy;2013 <a href='http://www.bss-ims.de'>B&amp;S Solutions - IT&amp;Media Services</a></p>
-        </div>
-	</div>
+<div class="container">
+    <div class="masthead">
+        <h3 class="text-muted"></h3>
+        <ul class="nav nav-justified">
+            <li class="active"><a href="#">Dashboard</a></li>
+            <li><a href="#">Customers</a></li>
+            <li><a href="#">Email</a></li>
+            <li><a href="#">Taskmanager</a></li>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">Admin</a></li>
+        </ul>
+    </div>
+    <!-- Site content -->
+    <div class="jumbotron">
+        <?php
+            include("pages/new_client/default.php");
+        ?>
+    </div>
+    <!-- Site footer -->
+    <div class="footer">
+        <p>&copy; 2013 <a href='http://www.bss-ims.de'>B&amp;S Solutions - IT&amp;Media Services</a></p>
+    </div>
+</div>
 </body>
 </html>
