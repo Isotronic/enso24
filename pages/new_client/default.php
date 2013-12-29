f8ede73ed0c7030cca3a5904ceff9625e43eb021
trigdzeen <trigdzeen@gmail.com>
2013-12-29 03:46:46 +0100

    a

--- pages/new_client/default.php
+++ pages/new_client/default.php
@@ -1,4 +1,65 @@
-
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+		$('#myModal').modal({
+				show: false,
+			  backdrop: 'static',
+			  keyboard: false  
+		});
+		
+		$("div#basic").css({"display":"block"});
+		$("div#loading").css({"display":"none"})
+		$("div#contact").css({"display":"none"});
+		$("div#address").css({"display":"none"});
+		
+		$("#submit").click(function(){
+				
+			var xmlhttp;
+			if(window.XMLHttpRequest)
+			{//code for ie7+, firefox, chrome, opera, safari
+				xmlhttp=new XMLHttpRequest();
+			}
+			else
+			{// code for IE6, IE5
+			  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange=function()
+			{
+				if(xmlhttp.readyState<4)
+				{
+					
+					$(".modal-body").css({
+							"opacity":"0.2",
+							"background-color":"#fff",
+							"color":"#000"
+					});
+					$("div#loading").css({"opacity":"1","display":"block"});
+				}
+				
+				if (xmlhttp.readyState==4 && xmlhttp.status==200)
+				{
+						document.getElementById("123").innerHTML=xmlhttp.responseText;
+						var stat=xmlhttp.responseText;
+						if(stat=="basic_added")
+						{
+							$(".modal-body").css({
+								"opacity":"1",
+								"background-color":"#fff",
+								"color":"#000"
+							});
+							$("div#basic").css({"display":"none"});
+							$("div#contact").css({"display":"block"});
+							$("div#loading").css({"display":"none"})
+							//$("div#address").css({"display":"none"})
+						}
+						xmlhttp.responseText="";
+				}
+			}
+			xmlhttp.open("GET","pages/new_client/form_handler.php",true);
+			xmlhttp.send();
+		});
+	});
+</script> 
 <div id="content">
 			<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
 			        	Add New Client</button>
@@ -15,11 +76,16 @@
 			        <?php
 			        	//this pulls the basic info form into this modal
 			        	include("basic.php");
+						include("contact.php");
+						include("address.php");
 			        ?>
+			        
 			        </div>
+			        <div id="loading"><img src="http://localhost/github/enso24/includes/images/laoding.gif" /></div>
+				        <div id="123">tshewang</div>
 			      <div class="modal-footer">
 			        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-			        <button type="button" class="btn btn-primary">Next</button>
+			        <button type="button" class="btn btn-primary" id="submit">Next</button>
 			      </div>
 			      
 			    </div><!-- /.modal-content -->
