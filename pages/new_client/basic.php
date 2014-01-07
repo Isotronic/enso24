d8342a9ca25b6f5733c112c3e9661b0034fc6fc1
trigdzeen <trigdzeen@gmail.com>
2014-01-07 13:31:55 +0100

    update

--- pages/new_client/basic.php
+++ pages/new_client/basic.php
@@ -9,6 +9,12 @@ Basic Information
 			<input type="text" name="first_name" id="first_name" placeholder="First Name"/><br />
 		<label for="last_name">Last Name </label>
 			<input type="text" name="last_name" id="last_name" placeholder="Last Name" /><br />
+			<label for="gender">Gender </label>
+			<select id="gender">
+				<option>Select Gender</option>
+				<option>Male</option>
+				<option>Female</option>
+			</select><br />
 		<label for="birth_date">Date of Birth </label>
 			<input type="date" name="birth_date" id="birth_date" /><br />
 			<input type="hidden" name="step" id="step" value="basic" />
